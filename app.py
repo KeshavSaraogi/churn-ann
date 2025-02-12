b975c23212fc8946e5d67ff1f80fe03b6031ec7c
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-02-12 20:52:16 +0530

    fix

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
+from keras.models import load_model
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5', compile = False)
+model = tf.keras.models.load_model('model.h5', compile = False, custom_objects={'InputLayer': tf.keras.layers.InputLayer})
 
 # Load the encoders and scaler
 with open('label_encoder_gender.pkl', 'rb') as file:
